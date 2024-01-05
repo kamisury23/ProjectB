ea8b87065defd2b072a90d693fa60bdc07abecd7
Karina <Karina@10.0.0.31>
2024-01-05 00:11:49 -0500

    Implemented isVacation ()

--- src/b/DemoB.java
+++ src/b/DemoB.java
@@ -4,9 +4,21 @@ public class DemoB {
 	public static void main(String[] args) {
 		
 		System.out.println("February");
-		
+	
+	//coming from local repo	
 		System.out.println("Monday, Tuesday...");
 		
+	}
+	
+	//coming from local repo
+	public boolean isVacation (String day, int time) {
+		boolean result = false;
+		
+		if (day == "Saturday" && time == 1) {
+			result = true;
+		}
+		
+		return result;
 	}
 
 }
